bbd86a60da0e871a853001ae7f1fcbe882810d89
snisioi <sergiu.nisioi@oracle.com>
2018-04-17 13:34:04 +0300

    update tcp client and server

---
+++ laborator2/src/tcp_server.py
@@ -0,0 +1,25 @@
+# TCP Server
+import socket
+import logging
+import time
+
+logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 10000
+adresa = 'localhost'
+server_address = (adresa, port)
+sock.bind(server_address)
+logging.info("Serverul a pornit pe %s si portnul portul %d", adresa, port)
+sock.listen(5)
+while True:
+    logging.info('Asteptam conexiui...')
+    conexiune, address = sock.accept()
+    logging.info("Handshake cu %s", address)
+    time.sleep(15)
+    data = sock.recv(1024)
+    logging.info('Content primit: "%s"', data)
+    conexiune.send("Server - OK")
+    conexiune.close()
+sock.close()
