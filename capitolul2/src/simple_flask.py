1a99e6e3c6c120db09043f6889893ef3ea0d7c21
senisioi <sergiu.nisioi@oracle.com>
2020-04-09 20:04:05 +0300

    pull latest submodule

--- capitolul2/src/simple_flask.py
+++ capitolul2/src/simple_flask.py
@@ -23,4 +23,4 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8008)
+    app.run(host='0.0.0.0', port=8001)
--- capitolul2/src/udp_server.py
+++ capitolul2/src/udp_server.py
@@ -7,7 +7,7 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
 
 port = 10000
-adresa = '0.0.0.0'
+adresa = 'localhost'
 server_address = (adresa, port)
 sock.bind(server_address)
 logging.info("Serverul a pornit pe %s si portnul portul %d", adresa, port)
