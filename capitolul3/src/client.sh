b147b7f4c3100018257ae0ba7fabcd69d3b3aa48
senisioi <sergiu.nisioi@oracle.com>
2020-05-12 18:15:10 +0300

    docker container as default gateway

--- capitolul3/src/client.sh
+++ capitolul3/src/client.sh
@@ -5,9 +5,9 @@ ip route del default
 # make router container the default router
 ip route add default via 172.10.0.1
 # add route to subnet 172.10.0.0/16 via IP 198.10.0.1
-ip route add 172.10.0.0/16 via 198.10.0.1
+# ip route add 172.10.0.0/16 via 198.10.0.1
 # add 8.8.8.8 nameserver
-echo "nameserver 8.8.8.8 \n" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 # we need to drop the kernel reset of hand-coded tcp connections
 # https://stackoverflow.com/a/8578541
 iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
