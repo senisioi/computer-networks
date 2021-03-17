69c4215a02d1cff68696950f069739bbc4435061
snisioi <sergiu.nisioi@gmail.com>
2021-03-17 18:08:38 +0200

    adding tema1 submodule and fixes in chap3

--- capitolul3/src/alter_packages.sh
+++ capitolul3/src/alter_packages.sh
@@ -3,11 +3,7 @@
 # more here https://www.cs.unm.edu/~crandall/netsfall13/TCtutorial.pdf
 # more here https://www.excentis.com/blog/use-linux-traffic-control-impairment-node-test-environment-part-2
 # we can put that on eth1 too:
-tc qdisc add dev eth0 root netem \ 
-								delay 100ms 10ms 25% \
-								loss 5% 25% \
-								corrupt 10% \
-								reorder 25% 50% && \
+tc qdisc add dev eth0 root netem delay 1000ms 10ms 25% loss 5% 25% corrupt 10% reorder 25% 50% && \
 sleep infinity
 
 
